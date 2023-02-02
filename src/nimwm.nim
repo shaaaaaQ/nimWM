bb4a660e2ae3095209156cfb14d41a3d061b14fc
sh <64454953+shaaaaaQ@users.noreply.github.com>
2023-02-02 13:24:35 +0900

    a

--- src/nimwm.nim
+++ src/nimwm.nim
@@ -14,14 +14,17 @@ proc initXWIndowInfo(winInfo: var XWindowInfo): XWIndowInfo =
   winInfo.display = XOpenDisplay(nil)
   if winInfo.display == nil:
     quit "Failed to open display"
-  
-  discard XGrabKey(winInfo.display, XKeysymToKeycode(winInfo.display, XStringToKeysym("F1")), Mod1Mask,
+
+  discard XGrabKey(winInfo.display, XKeysymToKeycode(winInfo.display,
+      XStringToKeysym("F1")), Mod1Mask,
     DefaultRootWindow(winInfo.display), true, GrabModeAsync, GrabModeAsync)
-  discard XGrabButton(winInfo.display, 1, Mod1Mask, DefaultRootWindow(winInfo.display), true,
+  discard XGrabButton(winInfo.display, 1, Mod1Mask, DefaultRootWindow(
+      winInfo.display), true,
     ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None)
-  discard XGrabButton(winInfo.display, 3, Mod1Mask, DefaultRootWindow(winInfo.display), true,
+  discard XGrabButton(winInfo.display, 3, Mod1Mask, DefaultRootWindow(
+      winInfo.display), true,
     ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None)
-  
+
   winInfo.start.subwindow = None
 
   return winInfo
@@ -29,12 +32,12 @@ proc initXWIndowInfo(winInfo: var XWindowInfo): XWIndowInfo =
 when isMainModule:
 
   var winInfo: XWindowInfo
-  
+
   winInfo = initXWIndowInfo(winInfo)
 
   while true:
     discard XNextEvent(winInfo.display, winInfo.ev.addr)
-  
+
     if winInfo.ev.theType == KeyPress:
       var kev = cast[PXKeyEvent](winInfo.ev.addr)[]
       if not kev.subwindow.addr.isNil:
@@ -46,7 +49,8 @@ when isMainModule:
         discard XGrabPointer(winInfo.display, bev.subwindow, true,
           PointerMotionMask or ButtonReleaseMask, GrabModeAsync,
           GrabModeAsync, None, None, CurrentTime)
-        discard XGetWindowAttributes(winInfo.display, bev.subwindow, winInfo.attr.addr);
+        discard XGetWindowAttributes(winInfo.display, bev.subwindow,
+            winInfo.attr.addr);
         winInfo.start = bev;
     elif winInfo.ev.theType == MotionNotify:
       var mnev = cast[PXMotionEvent](winInfo.ev.addr)[]
@@ -57,10 +61,12 @@ when isMainModule:
         xdiff = bev.x_root - winInfo.start.x_root
         ydiff = bev.y_root - winInfo.start.y_root
       discard XMoveResizeWindow(winInfo.display, mnev.window,
-        winInfo.attr.x + (if winInfo.start.button==1: xdiff else: 0),
-        winInfo.attr.y + (if winInfo.start.button==1: ydiff else: 0),
-        max(1, winInfo.attr.width + (if winInfo.start.button==3: xdiff else: 0)),
-        max(1, winInfo.attr.height + (if winInfo.start.button==3: ydiff else: 0)))
+        winInfo.attr.x + (if winInfo.start.button == 1: xdiff else: 0),
+        winInfo.attr.y + (if winInfo.start.button == 1: ydiff else: 0),
+        max(1, winInfo.attr.width + (if winInfo.start.button ==
+            3: xdiff else: 0)),
+        max(1, winInfo.attr.height + (if winInfo.start.button ==
+            3: ydiff else: 0)))
 
     elif winInfo.ev.theType == ButtonRelease:
       discard XUngrabPointer(winInfo.display, CurrentTime)
